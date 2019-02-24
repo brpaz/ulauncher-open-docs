cf410aeae393c60fc48b54ce7b6e69bcd815b26a
Bruno Paz <brunopaz@sapo.pt>
2019-02-24 15:37:44 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# ulauncher-open-docs
+
+[![Build Status](https://img.shields.io/travis/com/brpaz/ulauncher-open-docs.svg)](https://github.com/brpaz/ulauncher-open-docs)
+[![GitHub license](https://img.shields.io/github/license/brpaz/ulauncher-open-docs.svg)](https://github.com/brpaz/:ulauncher-open-docs/blob/master/LICENSE)
+
+> Open popular documnetation sites
+
+## Demo
+
+TODO Add demo here.
+
+## Included documentation
+
+* PHP
+* Symfony
+* Golang
+* NodeJS
+* Python
+* Netlify
+* GitLab
+* DigitalOcean
+* React
+* VueJS
+* Terraform
+* Ansbile
+* Docker
+* Git
+* and more...
+
+## Requirements
+
+* Ulauncher
+* Python >= 2.7
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```
+https://github.com/brpaz/ulauncher-open-docs
+```
+
+## Development
+
+```
+git clone https://github.com/brpaz/ulauncher-open-docs
+make link
+```
+
+The `make link` command will symlink the cloned repo into the appropriate location on the ulauncher extensions folder.
+
+To see your changes, stop ulauncher and run it from the command line with: `ulauncher -v`.
+
+## Contributing
+
+* Fork it!
+* Create your feature branch: git checkout -b my-new-feature
+* Commit your changes: git commit -am 'Add some feature'
+* Push to the branch: git push origin my-new-feature
+* Submit a pull request :D
+
+## License
+
+MIT &copy; [Bruno Paz]
---
+++ main.py
@@ -0,0 +1,68 @@
+"""
+Ulauncher Extension for opening popular documentation sites.
+"""
+
+import json
+import logging
+import os
+# pylint: disable=import-error
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+
+LOGGING = logging.getLogger(__name__)
+
+
+class OpenDocsExtension(Extension):
+    """ Main Extension Class  """
+
+    def __init__(self):
+        """ Initializes the extension """
+        super(OpenDocsExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+    def get_doc_icon(self, doc_name):
+        """ Returns the documentation icon with fallback for the default one if does not exists """
+        icon = 'images/docs/%s.png' % doc_name
+        icon_abs_path = os.path.join(os.path.dirname(__file__), icon)
+        if os.path.isfile(icon_abs_path):
+            return icon
+
+        return 'images/icon.png'
+
+
+class KeywordQueryEventListener(EventListener):
+    """ Listener that handles the user input """
+
+    # pylint: disable=unused-argument,no-self-use
+    def on_event(self, event, extension):
+        """ Handles the event """
+
+        with open('data/docs.json') as f_data:
+            docs = json.load(f_data)
+
+        query = event.get_argument() or ""
+
+        if query:
+            docs = [x for x in docs if query.lower() in x['name'].lower()]
+
+        docs = sorted(docs, key=lambda k: k['name'])
+        items = []
+
+        for doc in docs[:8]:
+
+            icon = extension.get_doc_icon(doc['key'])
+
+            items.append(ExtensionResultItem(
+                icon=icon,
+                name=doc['name'],
+                on_enter=OpenUrlAction(doc['url'])))
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    OpenDocsExtension().run()
